f0a5b8baac3d6ea53a210e262f68554545960c10
a13241 <a13241@d1-05-08.dped.local>
2018-02-06 15:42:36 +0000

    V1_ ficha Aval

--- src/m17b_fichaaval/Main.java
+++ src/m17b_fichaaval/Main.java
@@ -1,14 +1,21 @@
+//https://github.com/JotinhaPT/M17B_fichaAval
+
+/**
+ * Ficha de avaliação Main 
+ * @author João Marques & Martim Marreiros
+ * @version 1.0
+ */
 package m17b_fichaaval;
 
 public class Main {
 
     public static void main(String[] args) {
-        Ponto2D p1 = new Ponto2D(5,7);
-        Ponto2D p2 = new Ponto2D(2,8);
-        
+        Ponto2D p1 = new Ponto2D(5, 7);
+        Ponto2D p2 = new Ponto2D(2, 8);
+
         System.out.println(p1);
         System.out.println(p2);
-        
+
         System.out.println(p2.distancia(p1));
     }
 }
